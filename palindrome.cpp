8460d4625991a5f6b749684fc3c36e49a9d02b30
Muhammed Sabah <muhammedsabah@icloud.com>
2023-10-15 19:49:28 +0530

    Update palindrome.cpp
    Added a function to check whether string is Palindrome or not without using extra space.

--- palindrome.cpp
+++ palindrome.cpp
@@ -21,13 +21,24 @@ string isPalindrome(string S)
 	}
 }
 
+//Function to check if string is Palindrome without using extra space
+string isPalind(string &s){
+    	int l=0, r = s.size()-1;
+	while(l<r){
+		if (s[l++] != s[r--]) return "false";
+	}
+	return "true";
+
+}
+
 // Driver Code
 int main()
 {
-	string S;
+    string S;
     cout<<"Enter the string: ";
     cin>>S;
-	cout << isPalindrome(S);
-
-	return 0;
+    cout << isPalindrome(S);
+    cout<<isPalind(S);
+	
+    return 0;
 }
